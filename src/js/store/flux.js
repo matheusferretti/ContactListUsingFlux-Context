7432fbf31a7184adb0b5267e9cd54cd10b25741b
Matheus Monteiro <matheusferretti96@gmail.com>
2020-10-05 22:47:01 +0000

    updating

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,10 +2,29 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			loadContacts: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/Matheus_FerrettiMonteiro")
+					.then(function(response) {
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						// Read the response as json.
+						return response.json();
+					})
+					.then(function(responseAsJson) {
+						// Do stuff with the JSON
+						console.log("responseAsJson", responseAsJson);
+						setStore({ contacts: responseAsJson.results });
+					})
+					.catch(function(error) {
+						console.log("Looks like there was a problem: \n", error);
+					});
+			}
 		}
 	};
 };
